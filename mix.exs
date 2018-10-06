93cc470580d91f303c27e4c21c932e14afc971b0
Goran Pedić <gpedic@gmail.com>
2018-10-06 23:11:52 +0200

    Bump version to 2.0.0
    Just to bring it in line with the rest of the ex_aws ecosystem

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAwsSsm.MixProject do
   def project do
     [
       app: :ex_aws_ssm,
-      version: "0.1.0",
+      version: "2.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env),
