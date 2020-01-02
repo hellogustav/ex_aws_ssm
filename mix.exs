046571669fdd611aef0ca15c3d7e86a4c6df2693
Kamil Bielawski <kamil.bielawski@amberbit.com>
2020-01-02 16:35:26 +0100

    Bump ex_aws to 2.1

--- mix.exs
+++ mix.exs
@@ -70,7 +70,7 @@ defmodule ExAws.SSM.MixProject do
   defp ex_aws() do
     case System.get_env("AWS") do
       "LOCAL" -> {:ex_aws, path: "../ex_aws"}
-      _ -> {:ex_aws, "~> 2.0"}
+      _ -> {:ex_aws, "~> 2.1"}
     end
   end
 end
